7292bd2a7a8632466399e0ee628a127b3a836eee
Robert Tabacaru <49028058+robbyyt@users.noreply.github.com>
2024-10-04 00:13:02 +0300

    background and footer

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,3 +2,11 @@ export const SITE_TITLE = "Portfolio - Robert Tabacaru";
 export const SITE_DESCRIPTION = `Fullstack web development projects, posts and insights.
   Curated by Robert Tabacaru.
   `;
+
+export const SOCIALS = {
+  facebook: "https://www.facebook.com/robert.tabacaru.3/",
+  twitter: "https://twitter.com/robbyyt99",
+  github: "https://github.com/robbyyt",
+  email: "rtabacaru@gmail.com",
+  linkedin: "https://www.linkedin.com/in/rtabacaru/",
+};
